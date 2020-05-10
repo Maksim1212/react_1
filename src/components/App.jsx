b595ef144e99a8c4b318038fddb935f93bf38ebb
maksim1212 <mr.frost201611@gmail.com>
2020-05-10 12:42:39 +0300

    feat(components): was added list of films
    was added list of films

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {moviesData} from "../moviesData"
 
-function App() {
-  return <div>Hello React</div>;
+console.log(moviesData);
+
+//let title = 'Hello React';
+
+class App extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            movies: moviesData
+        }
+    }
+    render(){
+        console.log(this)
+        return <div>{this.state.movies.map(function(movie){
+        return <div><p>{movie.title}</p><button>Delete</button></div>
+        })}</div>;
+    }
 }
+
+// function App() {
+// return <div>{moviesData[0].title}</div>;
+// }
 
 export default App;
